b9ba3555e20eb03dd16d52f5f8eafd13bc4c7287
Cole Dennis <coleedennis@gmail.com>
2022-11-22 07:45:11 -0600

    Start building logic to build game board

--- ColoringAR/ARModel.swift
+++ ColoringAR/ARModel.swift
@@ -9,6 +9,7 @@ import Foundation
 import RealityKit
 
 struct ARModel {
+    var collectionOfCubes = [SIMD3(x: 0, y: 0, z: 0),SIMD3(x: 0.11, y: 0, z: 0), SIMD3(x: 0.22, y: 0.11, z: 0)]
     private(set) var arView : ARView
     
     init() {
@@ -19,8 +20,8 @@ struct ARModel {
     
     mutating func raycasting(location: CGPoint) {
         guard let query = arView.makeRaycastQuery(from: location/*arView.center*/,
-              allowing: .estimatedPlane,
-              alignment: .any)
+                                                  allowing: .estimatedPlane,
+                                                  alignment: .any)
         else { return }
         
         guard let result = arView.session.raycast(query).first
@@ -31,19 +32,49 @@ struct ARModel {
             print("tapping an existing square - \(nearestEntity.name)")
             
             nearestEntity.changeColor()
-
+            
         } else {
             
             let raycastAnchor = AnchorEntity(world: result.worldTransform)
             raycastAnchor.name = "raycast anchor"
             
-            let box = CustomBox(color: .red)
-            box.name = "custom box"
-            
-            raycastAnchor.addChild(box)
+            for box in placeGridOfCubes(parent: raycastAnchor) {
+                raycastAnchor.addChild(box)
+                print("added box = \(box.transform.translation)")
+            }
             
             arView.scene.anchors.append(raycastAnchor)
         }
+    }
+    
+    func placeGridOfCubes(parent: AnchorEntity) -> Array<Entity> {
+        
+        var array: Array<Entity> = []
+        
+        for item in collectionOfCubes {
+            let box = CustomBox(color: .white)
+            //                        box.name = "custom box"
+            box.transform.translation.x = Float(item.x)
+            box.transform.translation.y = Float(item.y)
+            box.transform.translation.z = Float(item.z)
+            array.append(box)
+        }
+        
+//        for xnum in 1...5 {
+//            for ynum in 1...5 {
+//                for znum in 1...5 {
+//                    if xnum > ynum || ynum > znum {
+//                        let box = CustomBox(color: .init(red: 1, green: 1, blue: 1, alpha: 1))
+//                        box.name = "custom box"
+//                        box.transform.translation.x = Float(0.11 * Float(xnum))
+//                        box.transform.translation.y = Float(0.11 * Float(ynum))
+//                        box.transform.translation.z = Float(0.11 * Float(znum))
+//                        array.append(box)
+//                    }
+//                }
+//            }
+//        }
+        return array
     }
     
 }
